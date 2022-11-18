3ae3cf4a6b6f1ed089e058a6a281a69fa84b9603
jauzanajwa <jauzacibinong11@gmail.com>
2022-11-18 20:52:23 +0700

    gi

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Models\Post;
+
+class PostController extends Controller
+{
+    public function index()
+    {
+        //get all posts from Model
+        $posts = Post::latest()->get();
+
+        //passing posts to view
+        return view('posts', compact('posts'));
+    }
+}
